396260af740a5ad495800129d8f9d8c6a8efcbb8
Anders Granås <anderog@stud.ntnu.no>
2020-02-25 15:39:15 +0100

    Add file for task 1 and finish 2b

--- RDD_task2.py
+++ RDD_task2.py
@@ -1,32 +1,19 @@
 from itertools import islice
-
 import pyspark
 
 reviewTablePath = "./yelp_top_reviewers_with_reviews.csv"
-businessTablePath = "./yelp_businesses.csv"
-friendshipGraphPath = "./yelp_top_users_friendship_graph.csv"
 
 conf = pyspark.SparkConf().setAppName("TDT4305-Project1").setMaster("local");
 # The master URL to connect to, such as "local" to run locally with one thread, "local[4]" to run locally with 4 cores, or "spark://master:7077" to run on a Spark standalone cluster.
 sc = pyspark.SparkContext(conf=conf)
 
-
-######## RDD Tasks ##########
 def create_rdd(path):
     return sc.textFile(path)
-
-
-# TASK 1
-def rdd_task1():
-    review_file = create_rdd(reviewTablePath)
-    business_file = create_rdd(businessTablePath)
-    friendship_file = create_rdd(friendshipGraphPath)
-
-    print("Number of rows:\n reviewTable: %i\n businessTable: %i\n friendshipTable: %i" % (review_file.count(), business_file.count(), friendship_file.count()))
 
 # TASK 2 - reviewTable
 def rdd_task2():
     review_file = create_rdd(reviewTablePath)
+    print(review_file.first())
     review_file = review_file.mapPartitionsWithIndex(lambda idx, it: islice(it, 1, None) if idx == 0 else it)
 
     # a)
@@ -34,6 +21,13 @@ def rdd_task2():
     # print("Number of distinct users: ", review_user_id.distinct().count())
 
     # b)
-    reviews = review_file.map(lambda line: line.split()[2])
-    reviews_length = reviews.map(lambda review: len(review))
+    review_text = review_file.map(lambda line: line.split()[3])
+    reviews_length = review_text.map(lambda review: len(review))
 
+    total = reviews_length.reduce(lambda x, y: x + y)
+    print(total)
+    average_length = total / reviews_length.count()
+    print("Average length of reviews: %i" % (average_length))
+
+
+rdd_task2()
