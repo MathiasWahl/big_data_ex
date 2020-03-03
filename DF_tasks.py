5c344b6fa36d37c09a4f0e964462918aca809df2
Mathias <ma10asw@gmail.com>
2020-03-03 14:43:46 +0100

    more cleanup of dataframe file

--- DF_tasks.py
+++ DF_tasks.py
@@ -1,21 +1,20 @@
-
 from pyspark.sql import SparkSession
 
 spark = SparkSession \
     .builder \
-    .appName("Exercises") \
-    .config("spark.some.config.option", "some-value") \
+    .appName("testAppForExercises") \
     .getOrCreate()
 
 
 # Task 5a
-def generate(display=False):
-    # read csv files and generate Frames
+def generate_frames(display=False):
+
+    # Read csv files and generate frames. Because InferSchema is set to true, the format of the values will be kept.
     business_frame = spark.read.format('csv').options(inferSchema='true', header='true', delimiter="	").load('yelp_businesses.csv')
     top_review_frame = spark.read.format('csv').options(header='true', inferSchema='true', delimiter="	").load('yelp_top_reviewers_with_reviews.csv')
     friendship_graph_frame = spark.read.format('csv').options(header='true', inferSchema='true').load('yelp_top_users_friendship_graph.csv')
 
-    # display frames
+    # Display frames (for testing)
     if display:
         business_frame.show()
         top_review_frame.show()
@@ -25,17 +24,20 @@ def generate(display=False):
 
 
 # Task 6
-def sql_queries(display=False):
-    business_frame, top_review_frame, friendship_graph_frame = generate()
+def do_sql_queries(display=False):
 
+    # Generate dataframes
+    business_frame, top_review_frame, friendship_graph_frame = generate_frames()
+
+    # Register the dataframes as SQL temporary views
     business_frame.createOrReplaceTempView("business")
     top_review_frame.createOrReplaceTempView("review")
 
-    # a) & b)
+    # Subtasks a) & b)
     sql_query_df = spark.sql("SELECT * FROM review INNER JOIN business ON review.business_id = business.business_id")
     sql_query_df.createOrReplaceTempView("results")
 
-    # c)
+    # Subtask c)
     top_20_reviewers_df = spark.sql("SELECT COUNT(review_id) as number_of_reviews, user_id FROM review GROUP BY user_id ORDER BY number_of_reviews DESC").limit(20)
 
     if display:
@@ -43,4 +45,5 @@ def sql_queries(display=False):
         top_20_reviewers_df.show()
 
 
-sql_queries(False)
+# Run all the code (do_sql_queries triggers generate_frames)
+do_sql_queries()
